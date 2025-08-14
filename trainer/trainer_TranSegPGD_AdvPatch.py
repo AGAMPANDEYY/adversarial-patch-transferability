8193a21fef7784523cb6576f3fc5457535b60a4f
Agam Pandey <94832116+AGAMPANDEYY@users.noreply.github.com>
2025-08-15 02:15:34 +0530

    Update trainer_TranSegPGD_AdvPatch.py

--- trainer/trainer_TranSegPGD_AdvPatch.py
+++ trainer/trainer_TranSegPGD_AdvPatch.py
@@ -92,7 +92,7 @@ class PatchTrainer():
       self.model.get()
 
       ## loss
-      self.criterion = PatchLoss(self.config)
+      self.criterion = PatchLoss(self.config, self.logger)
 
       ## optimizer
       # Initialize adversarial patch (random noise)
